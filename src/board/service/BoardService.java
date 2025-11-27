eadd69d4f3ebb91a2816830f7c235c0679b7359b
shoon0123 <82031787+shoon0123@users.noreply.github.com>
2025-11-27 10:07:57 +0900

    id로 글 조회 추가

--- src/board/service/BoardService.java
+++ src/board/service/BoardService.java
@@ -31,9 +31,8 @@ public class BoardService {
 		return dao.update(id,title,content,writer);
 	}
 	
-	public List<BoardVO> detailSelect(String title) {
-		return dao.detailSelect(title);
-		
+	public BoardVO detailSelect(int id) {
+		return dao.detailSelect(id);
 	}
 
 }
